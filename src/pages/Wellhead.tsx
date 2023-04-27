6c1454ae11831611e1704fcf8b04db53f20b6e60
zxjdzxb <1660154581@qq.com>
2023-04-27 11:21:55 +0800

    井字小游戏

--- src/pages/Wellhead.tsx
+++ src/pages/Wellhead.tsx
@@ -1,19 +1,17 @@
-//import s from './Wellhead.module.scss';
-
-
+import s from '@/styles/Wellhead.module.scss';
 import Layout from '@/components/Layout/Layout';
 import Square from '@/components/Square/Square';
 import {useState} from 'react';
 
 
-interface wellheadProps {
+interface boardProps {
   squares: Array<string>;
+  xIsNext: boolean;
+  onPlay: (nextSquares: Array<string>) => void;
 }
 
-export default function Wellhead() {
-  const [squares, setSquares] = useState<Array<string | null>>(Array(9).fill(null));
-  const [xIsNext, setXIsNext] = useState(true);
 
+function Board({xIsNext, squares, onPlay}: boardProps) {
   function handleClick(i: number) {
     if (calculateWinner(squares) || squares[i]) {
       return;
@@ -26,8 +24,7 @@ export default function Wellhead() {
     } else {
       nextSquares[i] = 'O';
     }
-    setSquares(nextSquares);
-    setXIsNext(!xIsNext);
+    onPlay(nextSquares);
   }
 
   const winner = calculateWinner(squares);
@@ -37,30 +34,71 @@ export default function Wellhead() {
   } else {
     status = 'Next player: ' + (xIsNext ? 'X' : 'O');
   }
-
   return (
     <>
-      <Layout title="井字小游戏">
-        <div className="status">{status}</div>
-        <div className="board-row">
-          <Square value={squares[0]} onSquareClick={() => handleClick(0)}/>
-          <Square value={squares[1]} onSquareClick={() => handleClick(1)}/>
-          <Square value={squares[2]} onSquareClick={() => handleClick(2)}/>
-        </div>
-        <div className="board-row">
-          <Square value={squares[3]} onSquareClick={() => handleClick(3)}/>
-          <Square value={squares[4]} onSquareClick={() => handleClick(4)}/>
-          <Square value={squares[5]} onSquareClick={() => handleClick(5)}/>
-        </div>
-        <div className="board-row">
-          <Square value={squares[6]} onSquareClick={() => handleClick(6)}/>
-          <Square value={squares[7]} onSquareClick={() => handleClick(7)}/>
-          <Square value={squares[8]} onSquareClick={() => handleClick(8)}/>
-        </div>
-      </Layout>
 
+      <div className={s.status}>{status}</div>
+      <div className={s.board_row}>
+        <Square value={squares[0]} onSquareClick={() => handleClick(0)}/>
+        <Square value={squares[1]} onSquareClick={() => handleClick(1)}/>
+        <Square value={squares[2]} onSquareClick={() => handleClick(2)}/>
+      </div>
+      <div className={s.board_row}>
+        <Square value={squares[3]} onSquareClick={() => handleClick(3)}/>
+        <Square value={squares[4]} onSquareClick={() => handleClick(4)}/>
+        <Square value={squares[5]} onSquareClick={() => handleClick(5)}/>
+      </div>
+      <div className={s.board_row}>
+        <Square value={squares[6]} onSquareClick={() => handleClick(6)}/>
+        <Square value={squares[7]} onSquareClick={() => handleClick(7)}/>
+        <Square value={squares[8]} onSquareClick={() => handleClick(8)}/>
+      </div>
 
     </>
+  );
+}
+
+
+export default function Game() {
+  const [history, setHistory] = useState([Array(9).fill(null)]);
+  const [currentMove, setCurrentMove] = useState(0);
+  const xIsNext = currentMove % 2 === 0;
+  const currentSquares = history[currentMove];
+
+  function handlePlay(nextSquares: Array<string>) {
+    const nextHistory = [...history.slice(0, currentMove + 1), nextSquares];
+    setHistory(nextHistory);
+    setCurrentMove(nextHistory.length - 1);
+  }
+
+  function jumpTo(nextMove: number) {
+    setCurrentMove(nextMove);
+  }
+
+  const moves = history.map((squares, move) => {
+    let description;
+    if (move > 0) {
+      description = 'Go to move #' + move;
+    } else {
+      description = 'Go to game start';
+    }
+    return (
+      <li key={move}>
+        <button className={s.li} onClick={() => jumpTo(move)}>{description}</button>
+      </li>
+    );
+  });
+  return (
+    <Layout title="井字小游戏">
+      <div className={s.game}>
+        <div className={s.game_board}>
+          <Board xIsNext={xIsNext} squares={currentSquares} onPlay={handlePlay}/>
+        </div>
+        <div className={s.game_info}>
+          <ol className={s.ol}>{moves}</ol>
+        </div>
+      </div>
+    </Layout>
   );
 }
 
